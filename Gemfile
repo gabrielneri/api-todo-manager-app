e0edb68e3db9b6df6d625ecca02acab7f142c21d
Gabriel <gasantosneri@hotmail.com>
2023-08-10 16:43:34 -0300

    Add user auth initial config

--- Gemfile
+++ Gemfile
@@ -46,3 +46,7 @@ group :development do
   # gem "spring"
 end
 
+
+gem "devise_token_auth", "~> 1.2"
+
+gem "active_model_serializers", "~> 0.10.13"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
+    mount_devise_token_auth_for 'User', at: 'auth'
     resources :tasks
   end
 end
